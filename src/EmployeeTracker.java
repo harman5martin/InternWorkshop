b0653efca0191eaf5279bd9cc189d38a7b17642a
Harman Martin <harmanmartin@Harmans-MacBook-Pro.local>
2020-07-15 11:46:11 -0400

    singleton example

---
+++ src/EmployeeTracker.java
@@ -0,0 +1,22 @@
+package src.src;
+
+import java.util.List;
+
+public class SingletonExample {
+    private List<Employee> employees;
+
+    private SingletonExample() {
+        employees = new List<Employee>;
+    }
+
+    public void addEmployee(Employee employee) {
+        employees.add(employee);
+    }
+
+    private static SingletonExample
+
+
+
+
+
+}
