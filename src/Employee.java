d5b3eeaf46a4f71024861643130bdd21e95a3c7b
Harman Martin <harmanmartin@Harmans-MacBook-Pro.local>
2020-07-15 13:14:26 -0400

    multiton done

--- src/Employee.java
+++ src/Employee.java
@@ -1,4 +1,41 @@
 package src.src;
 
+import java.util.HashMap;
+import java.util.Map;
+
+// Multiton example
 public class Employee {
+    private String name;
+    private String role;
+    private int employeeId;
+
+    // note the private constructor,
+    // only can be called within this class
+    private Employee(String name, String role, int employeeId) {
+        this.name = name;
+        this.role = role;
+        this.employeeId = employeeId;
+    }
+
+    public String getName() {return name;}
+    public String getRole() {return role;}
+    public int getId() {return employeeId;}
+
+    // Private static variable storing all of the employees
+    private static Map<Integer, Employee> employees = new HashMap<>();
+
+    public static Employee getEmployee(String name, String role, int employeeId) {
+       // Checks whether or not an employee exists or not
+        if (employees.containsKey(employeeId)) {
+            // if employee exists with this id,
+            // then just returns it
+            return employees.get(employeeId);
+        } else {
+            // if no employee exists with this id,
+            // then create one and return it
+            Employee newEmployee = new Employee(name, role, employeeId);
+            employees.put(employeeId, newEmployee);
+            return newEmployee;
+        }
+    }
 }
--- src/EmployeeTracker.java
+++ src/EmployeeTracker.java
@@ -1,22 +1,33 @@
 package src.src;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class SingletonExample {
-    private List<Employee> employees;
+// Singleton Example
+public class EmployeeTracker {
+    private final List<Employee> employees;
 
-    private SingletonExample() {
-        employees = new List<Employee>;
+    // note the private constructor,
+    // only can be called in this class
+    private EmployeeTracker() {
+        employees = new ArrayList<Employee>();
     }
 
     public void addEmployee(Employee employee) {
         employees.add(employee);
     }
 
-    private static SingletonExample
+    private static EmployeeTracker employeeTracker;
 
-
-
-
-
+    public static EmployeeTracker getEmployeeTracker() {
+        if (employeeTracker == null) {
+            // Currently no EmployeeTracker exists,
+            // so we create one here
+            employeeTracker = new EmployeeTracker();
+        }
+        // If EmployeeTracker is not null, aka it already exists,
+        // we simply return the current EmployeeTracker
+        return employeeTracker;
+    }
 }
+
