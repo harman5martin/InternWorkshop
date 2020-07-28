b27b09284f340221c933ce1c48d4196a473151f4
Harman Martin <harmanmartin@Harmans-MacBook-Pro.local>
2020-07-28 15:55:31 -0400

    updated

--- src/ObserverObservableExample/Main.java
+++ src/ObserverObservableExample/Main.java
@@ -15,4 +15,16 @@ public class Main {
         // new Game(<sport>, <team1>, <team2>
         // new Game("Basketball", "Duke", "UNC");
     }
+
+    // Scanner directions and example
+    // When typing into the console information about the games and score, do so in
+    // the following format: 1) game index for the game you want (int), 2) the team
+    // that scored (string) (MUST match the exact string of the team when you create the game),
+    // 3) amount of points scored at that instance (int).
+    // Note: There MUST be a space in between each of the 3 inputs when entering them into
+    // the console. Also, pressing enter in between each one is fine too.
+    // Example1: 0 UNC 3
+    // Example2: 1
+    //           DUKE
+    //           2
 }
