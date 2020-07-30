fca970bcde24c7c4677100d8c6115a385fd0b0f7
Harman Martin <harmanmartin@Harmans-MacBook-Pro.local>
2020-07-30 15:27:04 -0400

    ex1 updated

--- src/IteratorExample/RatedPlayerIterator.java
+++ src/IteratorExample/RatedPlayerIterator.java
@@ -2,23 +2,17 @@ package src.src.IteratorExample;
 
 public class RatedPlayerIterator {
 
-    // TODO Exercise 1
-
-    // Create a custom iterator that encapsulates a list of players
-    // and iterates through it based on the player's rating.
-    // In this case, the iterator should only return players with
-    // a 4 star rating or higher. This implementation does NOT need a
-    // remove method.
-
-    // After you complete the iterator, create a main class in the package
-    // titled "IteratorExample" that uses this iterator and prints out
-    // to the console the players information like the output of BasicIterator.
-
-    // If working correctly, you should only see players printed that have 4 or 5 stars.
-
-    // Feel free to ask any questions that arise during the exercise and be ready to share
-    // your implementation with the group when we reconvene after the exercise.
-
-    // Note: I have my own solution to this exercise in another repository that will be shared
-    // to everyone after each group is provided time to work on this exercise.
 }
+
+
+// TODO Exercise 1
+
+// Create a custom iterator that encapsulates a list of players
+// and iterates through it based on the player's rating.
+// In this case, the iterator should only return players with
+// a 4 star rating or higher. This implementation does NOT need a
+// remove method.
+
+// After you complete the iterator, create a main class in the package
+// titled "IteratorExample" that uses this iterator and prints out
+// to the console the players information like the output of BasicIterator.
